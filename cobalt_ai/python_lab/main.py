b6eb27798061995bbee75887d1c7e719bd1bdff0
Ibrahim <98814268+ibrahim-3595@users.noreply.github.com>
2026-04-15 12:14:39 +0530

    deleted reedundant files for AI, modified main.py

--- cobalt_ai/python_lab/main.py
+++ cobalt_ai/python_lab/main.py
@@ -1,14 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scratch_model import LinearModel
+print("hello world")
 
-model = LinearModel()
-print(model.predict(5))
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scratch_model import LinearModel
 
-# simple dataset
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([2, 4, 6, 8, 10])
+# model = LinearModel()
+# print(model.predict(5))
 
-plt.scatter(x, y)
-plt.title("Dummy Data")
-plt.show()
+# # simple dataset
+# x = np.array([1, 2, 3, 4, 5])
+# y = np.array([2, 4, 6, 8, 10])
+
+# plt.scatter(x, y)
+# plt.title("Dummy Data")
+# plt.show()

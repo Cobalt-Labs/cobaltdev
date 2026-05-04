0decbf2f0b720a3d4320de5fb0662d196e02e174
Ibrahim <98814268+ibrahim-3595@users.noreply.github.com>
2026-05-04 12:22:46 +0530

    enhanced contact page

--- cobalt_frontend/web/cobaltweb/lib/pages/contact_page.dart
+++ cobalt_frontend/web/cobaltweb/lib/pages/contact_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import '../widgets/animated_section.dart';
 import '../widgets/glass_card.dart';
 
@@ -54,6 +52,7 @@ class _ContactPageState extends State<ContactPage> {
       
       setState(() => successMessage = "Opening email client...");
       _clearFields();
+      _showSuccessDialog();
     } catch (e) {
       setState(() => errorMessage = "Could not open email client. Please use the buttons below.");
     }

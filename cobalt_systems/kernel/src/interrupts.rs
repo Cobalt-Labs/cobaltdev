0a8fcc6486619a2e0eac4a1dfaf05f9e31168d67
Ibrahim <98814268+ibrahim-3595@users.noreply.github.com>
2026-05-09 14:36:09 +0530

    Fix interrupt descriptor table indexing to use u8

--- cobalt_systems/kernel/src/interrupts.rs
+++ cobalt_systems/kernel/src/interrupts.rs
@@ -36,8 +36,8 @@ static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
             .set_handler_fn(double_fault_handler)
             .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
     }
-    idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-    idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+    idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
+    idt[InterruptIndex::Keyboard.as_u8()].set_handler_fn(keyboard_interrupt_handler);
     idt
 });
 

95ca7fa2c2696810af5c6fef0676657e24eb8b26
Ibrahim <98814268+ibrahim-3595@users.noreply.github.com>
2026-07-09 17:25:44 +0530

    new: init udp protocol

--- cobalt_backend/server/udp/src/main.rs
+++ cobalt_backend/server/udp/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use crate::{udp_client::udp_client, udp_server::udp_server};
+
+mod udp_server;
+mod udp_client;
+
+fn main() -> std::io::Result<()> {
+    let server = udp_server()?;
+    let client = udp_client()?;
+    println!("{server:?} {client:?}");
+
+    Ok(())
 }
